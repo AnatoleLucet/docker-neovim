62b4a1a5f3629a887a31e9a1cdfc7847af64006f
Michael Vigor <m@michaelvigor.dev>
2021-06-09 21:14:24 +0100

    Create ubuntu and alpine version for each image build

--- .github/actions/release/run.sh
+++ .github/actions/release/run.sh
@@ -4,10 +4,13 @@
 export IS_EXISTENT=$([ $(curl --silent -f -lSL https://index.docker.io/v1/repositories/anatolelucet/neovim/tags/${TAG:-$TARGET} 2> /dev/null) ] && echo true || echo false)
 [ "$ALLOW_OVERRIDE" = "false" ] && [ "$IS_EXISTENT" = "true" ] && echo "Cannot override the existing image. Exiting." && exit 0 || true
 
-export IMAGE_NAME=anatolelucet/neovim:${TAG:-$TARGET}
+export ALPINE_IMAGE_NAME=anatolelucet/neovim:${TAG:-$TARGET}
+export UBUNTU_IMAGE_NAME=anatolelucet/neovim:${TAG:-$TARGET}-ubuntu
 
-docker build -f Dockerfile --build-arg TARGET=${TARGET:-TAG} -t $IMAGE_NAME .
+docker build -f alpine/Dockerfile --build-arg TARGET=${TARGET:-TAG} -t $ALPINE_IMAGE_NAME .
+docker build -f ubuntu/Dockerfile --build-arg TARGET=${TARGET:-TAG} -t $UBUNTU_IMAGE_NAME .
 
 docker login -u anatolelucet -p $PASSWORD
 
-docker push $IMAGE_NAME
+docker push $ALPINE_IMAGE_NAME
+docker push $UBUNTU_IMAGE_NAME
