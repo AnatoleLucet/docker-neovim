45d7b240f8a66e379360e1f4b131434b24b8006c
AnatoleLucet <anatole.lucet@proton.me>
2025-07-23 13:45:49 +0200

    feat: support arm64

--- .github/actions/release/run.sh
+++ .github/actions/release/run.sh
@@ -3,16 +3,14 @@
 set -e
 
 # Abort if the image already exists on Docker Hub
-export IS_EXISTENT=$([ $(curl --silent -f -lSL https://index.docker.io/v1/repositories/anatolelucet/neovim/tags/${TAG:-$TARGET} 2> /dev/null) ] && echo true || echo false)
-[ "$ALLOW_OVERRIDE" = "false" ] && [ "$IS_EXISTENT" = "true" ] && echo "Cannot override the existing image. Exiting." && exit 0 || true
+export EXISTS=$([ $(curl --silent -f -lSL https://index.docker.io/v1/repositories/anatolelucet/neovim/tags/${TAG:-$VERSION} 2> /dev/null) ] && echo true || echo false)
+[ "$ALLOW_OVERRIDE" = "false" ] && [ "$EXISTS" = "true" ] && echo "Cannot override an existing image. Exiting." && exit 0 || true
 
-export ALPINE_IMAGE_NAME=anatolelucet/neovim:${TAG:-$TARGET}
-export UBUNTU_IMAGE_NAME=anatolelucet/neovim:${TAG:-$TARGET}-ubuntu
-
-docker build -f alpine/Dockerfile --build-arg TARGET=${TARGET:-TAG} -t $ALPINE_IMAGE_NAME .
-docker build -f ubuntu/Dockerfile --build-arg TARGET=${TARGET:-TAG} -t $UBUNTU_IMAGE_NAME .
+export ALPINE_IMAGE_NAME=anatolelucet/neovim:${TAG:-$VERSION}
+export UBUNTU_IMAGE_NAME=anatolelucet/neovim:${TAG:-$VERSION}-ubuntu
 
 docker login -u anatolelucet -p $PASSWORD
 
-docker push $ALPINE_IMAGE_NAME
-docker push $UBUNTU_IMAGE_NAME
+docker buildx build --push --platform linux/amd64,linux/arm64 -f alpine/Dockerfile --build-arg VERSION=${VERSION} -t $ALPINE_IMAGE_NAME .
+docker buildx build --push --platform linux/amd64,linux/arm64 -f ubuntu/Dockerfile --build-arg VERSION=${VERSION} -t $UBUNTU_IMAGE_NAME .
+
